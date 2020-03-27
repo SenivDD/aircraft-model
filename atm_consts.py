ad3febf860342609a46e5ef8bc9d5a5b5adac94f
dextrenvy <31600348+dextrenvy@users.noreply.github.com>
2020-03-27 16:42:17 +0300

    Add files via upload
    The prototype of the class model of the earth's atmosphere

---
+++ atm_consts.py
@@ -0,0 +1,8 @@
+c_MolarMass = 28.964420
+c_AvogadroNumber = 602.257 * (10 ** 24)
+c_UniversalGasConst = 8314.32
+c_SpecificGasConst = 287.05287
+c_AdiabaticExp = 1.4
+c_ConventionalRadius = 6356767
+c_AccelerationOfGravity = 9.80665
+c_KelvinTemp = 273.15
