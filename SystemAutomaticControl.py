835944f8503782f07ed50951eb0f0a95bc069615
ZheleznovIlia <62719911+ZheleznovIlia@users.noreply.github.com>
2020-04-04 12:04:42 +0300

    Update SystemAutomaticControl.py
    v0.2.1. По сути 0.2 с небольшими дополнениями.

--- SystemAutomaticControl.py
+++ SystemAutomaticControl.py
@@ -2,80 +2,94 @@ import numpy as np
 from model_function_upr import *
 from matplotlib import pyplot as plt
 
+array_eliv = []     # массив значений отклонения руля (угол отклонения руля [град])
+array_ny_now = []   # массив значений перегрузок в данный момент времени
+array_ny_spec = []  # массив значений заданной перегрузки (постоянная)
+array_t = []        # массив значений времени
 
-#ny_tek = 0 # начальное значение перегрузки
-#ny_zad = 3
-#k = 0.01
+eps_ny = 0.01       # |(ny_now - ny_spec)| > eps_ny
 
-#def get_angle_wheel (self) :
+# Функция получения положения руля высоты и текущей перегрузки 
+#   ny_spec - заданное значение перегрузки   
+def get_eliv_and_ny_new (ny_spec) :
     
-ny_tek = 0 # начальное значение перегрузки
-eliv_new = 0 # начальное положение угла руля высоты
-ny_zad = 10
-k = 0.001
-t = 0
-i = 0
-y_eliv_new = []
-y_ny_tek = []
-y_ny_zad = []
-x_t = []
+    global array_eliv
+    global array_ny_now
+    global array_ny_spec
+    global array_t
 
-output = open("output.txt", "w")
-
-output.write("t         eliv_new  ny_tek \n")
-
-while ((abs(ny_tek - ny_zad) > 0.01) and (t <= 10)) :
-#while (ny_tek != ny_zad) :
+    array_eliv.clear()
+    array_ny_now.clear()
+    array_ny_spec.clear()
+    array_t.clear()
     
-    x_t.append(t)
-    y_eliv_new.append(eliv_new*180.0/np.pi)
-    y_ny_tek.append(ny_tek)
-    y_ny_zad.append(ny_zad)
+    ny_now = 0 # начальное значение перегрузки
+    eliv_new = 0 # начальное положение угла руля высоты
+    k = 0.001
+    t = 0
+    i = 0
 
-    #print ('eliv_new = ', eliv_new)
-    #print ('ny_tek = ', ny_tek)
-    #print ('t = ', t)
-    #print ('')
-
-    output.write ( "{:f}  {:f}  {:f}\n".format(t, eliv_new*180.0/np.pi, ny_tek) )
-
-    #eliv_new = eliv_last + k*(ny_zad-ny_tek)
-    ny_tek = get_ny(eliv_new)/ACCELERATION_OF_GRAVITY
-    eliv_new = eliv_new + k*(ny_zad-ny_tek)
-
-    if (eliv_new* 180.0/np.pi >= 26) :
-        eliv_new = 26/180.0*np.pi
-
-    if (eliv_new* 180.0/np.pi <= -28) :
-        eliv_new = -28/180.0*np.pi
-    #eliv_new = eliv_new * 180.0/np.pi
+    while ((abs(ny_now - ny_spec) > eps_ny) and (t <= 10)) :
+    #while (ny_now != ny_spec) :
     
-    #eliv_last = eliv_new
-    t = t + 0.02
+        array_t.append(t)
+        array_eliv.append(eliv_new*180.0/np.pi)
+        array_ny_now.append(ny_now)
+        array_ny_spec.append(ny_spec)
 
+        ny_now = get_ny(eliv_new) / ACCELERATION_OF_GRAVITY
+        eliv_new = eliv_new + k * (ny_spec - ny_now)
+
+        if (eliv_new * 180.0/np.pi >= 26) :
+            eliv_new = 26/180.0*np.pi
+
+        if (eliv_new * 180.0/np.pi <= -28) :
+            eliv_new = -28/180.0*np.pi
+
+        t = t + 0.02
+        
+    return eliv_new, array_eliv
+
+# Функция записи в файл полученных результатов 
+#   filename - имя файла для записи
+#   ny_spec - заданное значение перегрузки 
+def WriteToFile (filename, ny_spec) :
+   
+    get_eliv_and_ny_new (ny_spec)
+   
+    np.savetxt(filename, 
+        np.column_stack((array_t, array_eliv, array_ny_now)), 
+        fmt = '%5.2f | %8.5f | %8.5f', 
+        header = ('Для перегрузки = ' + str(ny_spec) + '\nt   | eliv_new |  ny_now\n-------------------------'), 
+        delimiter = ' | ')
     
-
-    #print ('eliv_last = ', eliv_last)
-
-
-#fig = plt.figure()
-output.close()
-
+    return
+    
 fig1 = plt.figure()
 plt.title("Изменение отклонения руля высоты")
 plt.xlabel ("Время наблюдения, [c]")
 plt.ylabel ("Угол текущего положения рулей, [град]")
-plt.plot (x_t, y_eliv_new)
+
+for i in range(1, 6, 2): # цикл от 1 до 5 с шагом 2, 
+    #i - значения перегрузки
+    get_eliv_and_ny_new (i)
+    plt.plot (array_t, array_eliv, label = 'Заданная перегрузка = ' + str(i))
+    print("\ni = ", i)
+
+plt.legend()
 plt.grid()
 
 fig2 = plt.figure()
+get_eliv_and_ny_new (1)
 plt.title("Изменение значения перегрузки")
 plt.xlabel ("Время наблюдения, [c]")
 plt.ylabel ("Перегрузка, []")
-plt.plot (x_t, y_ny_tek)
-plt.plot (x_t, y_ny_zad)
+plt.plot (array_t, array_ny_now, label = 'Текущая перегрузка')
+plt.plot (array_t, array_ny_spec, label = 'Заданная перегрузка')
+plt.legend()
 plt.grid()
-
 plt.show()
-           
- #   return eliv_new
+
+WriteToFile ("output1.txt", 1)
+WriteToFile ("output3.txt", 3)
+WriteToFile ("output5.txt", 5)
